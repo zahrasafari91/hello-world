3682a167506b1e49dfe9d9ce8289c8606e097973
zahrasafari91 <59757499+zahrasafari91@users.noreply.github.com>
2020-01-20 09:10:16 -0700

    Add files via upload

---
+++ JavaScript/index.js
@@ -0,0 +1,22 @@
+console.log("Hello World");
+
+const bob = "bob";
+console.log("bob")
+
+let lotto = "0000000";
+
+let myfriend = "Bob";
+
+console.log(lotto,myfriend);
+
+const arr = [0, 0, 0, 0, 0];
+
+const obj = {
+    name: "zahra",
+    age: 28,
+    gender: "female",
+    class: ["sodv","tech1101ß"]
+};
+
+// console.log(obj)
+
